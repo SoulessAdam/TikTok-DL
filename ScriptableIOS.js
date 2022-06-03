101d53a20c2b418b3f8d432fe5cf45336524d859
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2022-06-03 02:03:16 +0100

    Added iOS Scriptable

---
+++ ScriptableIOS.js
@@ -0,0 +1,20 @@
+// Scriptable iOS Script to grab url from share sheet and automatically redirect to download url.
+var apiEndpoint = "https://t774.herokuapp.com/"
+var apiResponse = new Request(apiEndpoint)
+apiResponse.method = "POST";
+apiResponse.headers = {
+    "Content-type": "application/json",
+    "Accept": "*/*"
+}
+apiResponse.body = JSON.stringify({
+    "url": args.urls[0]
+});
+var jsonR = await apiResponse.loadJSON();
+var url = jsonR["url"]
+if(url != undefined) Safari.open(url)
+else{
+    noti = new Notification()
+    noti.title = "Error"
+    noti.body += jsonR["error"]
+    noti.schedule()
+}
